c5c1a41f724e8abf440dcde555f313c9ffd52504
Emmanuel Cazenave <emmanuel.cazenave@scality.com>
2015-02-16 17:43:01 +0100

    Configure devstack.

--- 10-install-deps.sh
+++ 10-install-deps.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -xue
 
 git clone -b ${DEVSTACK_BRANCH} https://github.com/openstack-dev/devstack.git
+cd devstack
+cp ./samples/local.conf local.conf
+cat >> local.conf <<EOF
+disable_all_services
+enable_service key mysql s-proxy s-object s-container s-account
+EOF
